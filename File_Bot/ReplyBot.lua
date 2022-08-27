0f1d7c6772eb825b84729d9cfb8a7abccb0339d3
MilanuuTEAM <93672704+MilanuuTEAM@users.noreply.github.com>
2022-08-27 23:46:25 +0300

    Update ReplyBot.lua

--- File_Bot/ReplyBot.lua
+++ File_Bot/ReplyBot.lua
@@ -1,5 +1,16 @@
-local function Reply(msg)
-local text = msg.content.text.text
+function ReplyBot(msg)
+if msg.content.voice_note then 
+local File = json:decode(https.request('https://api.telegram.org/bot'..Token..'/getfile?file_id='..msg.content.voice_note.voice.remote.id))
+local get = io.popen('curl -s "https://fastbotss.herokuapp.com/yt?vi=https://api.telegram.org/file/bot'..Token..'/'..File.result.file_path..'"'):read('*a')
+local json = JSON.decode(get)
+if json and json.text then
+text = json.text
+end
+elseif msg.content.text then
+text = msg.content.text.text
+else 
+text = nil
+end
 if text== "انجب" or text== "نجب" or text=="انجبي" or text=="نجبي" then
 if msg.ControllerBot then  
 return merolua.sendText(msg.chat_id,msg.id,"تامر امر سيادة المطور تاج راسي 😊")
